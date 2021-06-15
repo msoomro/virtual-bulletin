646afc88c2bd3e8c84225a8a8ee10046eaeae765
Moomal <moomalmsoomro@gmail.com>
2021-06-15 06:18:06 -0400

    Add 'create new board' feature

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,11 +1,23 @@
 class BoardsController < ApplicationController
 
+    #new
+    get "/boards/new" do
+        erb :"boards/new.html"
+    end
+    
+    #create
+    post "/boards" do
+        @board = current_user.boards.create(params[:board])
+        redirect "/boards/#{@board.id}"
+    end
+    
     #index
     get "/boards" do
         @boards = Board.all
         erb :"boards/index.html"
     end
 
+    #show
     get "/boards/:id" do
         @board = Board.find(params[:id])
         erb :"/boards/show.html"
