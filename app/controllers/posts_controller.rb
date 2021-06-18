7ff31db799bf1a2b6bdde870cb023f86fdf0db20
Moomal <moomalmsoomro@gmail.com>
2021-06-18 07:55:27 -0400

    Add 'create a post' functionality by creating the Posts create route

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,12 @@
 class PostsController < ApplicationController
 
+    # create
+    post "/boards/:board_id/posts" do
+        @board = Board.find(params[:board_id])
+        @post = @board.posts.new(params[:post])
+        @post.user_id = current_user.id
+        @post.save
+        redirect "/boards/#{@board.id}"
+    end
+
 end
