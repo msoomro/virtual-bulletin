4c42847b16caedb0e8a46dcc52df16749c71fcd3
Moomal <moomalmsoomro@gmail.com>
2021-06-18 16:03:34 -0400

    Add validations to require presence of username and password when signing up

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ActiveRecord::Base
     has_many :posts, through: :boards
     validates :username, uniqueness: true
     validates :password, length: { in: 6..50 }
+    validates :username, presence: :true
+    validates :password, presence: :true
 end
