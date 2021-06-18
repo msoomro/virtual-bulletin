32169df4d4291163e363549f87410688a44703f2
Moomal <moomalmsoomro@gmail.com>
2021-06-18 08:12:29 -0400

    Add validations to posts controller so only logged in users can make posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,11 +2,15 @@ class PostsController < ApplicationController
 
     # create
     post "/boards/:board_id/posts" do
-        @board = Board.find(params[:board_id])
-        @post = @board.posts.new(params[:post])
-        @post.user_id = current_user.id
-        @post.save
-        redirect "/boards/#{@board.id}"
+        if logged_in?
+            @board = Board.find(params[:board_id])
+            @post = @board.posts.new(params[:post])
+            @post.user_id = current_user.id
+            @post.save
+            redirect "/boards/#{@board.id}"
+        else
+            redirect_if_not_logged_in
+        end
     end
 
 end
