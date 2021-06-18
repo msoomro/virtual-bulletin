ed22b02612f9e91440e446322fc0aa6088b780ed
Moomal <moomalmsoomro@gmail.com>
2021-06-18 08:00:17 -0400

    Add validations to Post model to require presence of: title, user_id, and board_id

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ActiveRecord::Base
     belongs_to :board
     belongs_to :user
+    validates :title, presence: :true
+    validates :board_id, presence: :true
+    validates :user_id, presence: :true
 end
